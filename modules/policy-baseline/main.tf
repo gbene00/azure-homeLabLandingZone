c8415627caeaabddc0d891273555e989ddde1987
gbene <bene.gabriel3@gmail.com>
2025-12-29 09:24:51 +0200

    UUID changed

--- modules/policy-baseline/main.tf
+++ modules/policy-baseline/main.tf
@@ -1,26 +1,33 @@
 locals {
-  built_in_policy_definition_ids = {
-    allowed_locations        = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
-    require_tag_on_resources = "/providers/Microsoft.Authorization/policyDefinitions/871b8d14-10aa-478d-b590-94f262ecfa99"
+  ## Map our "policy_type" to built-in policy display names
+  built_in_policy_display_names = {
+    allowed_locations        = "Allowed locations"
+    require_tag_on_resources = "Require a tag on resources"
   }
 
-  policy_definition_id = try(local.built_in_policy_definition_ids[var.policy_type], null)
+  policy_display_name = try(local.built_in_policy_display_names[var.policy_type], null)
+
+  ## Accept either UUID or full resource ID
+  subscription_resource_id = startswith(var.subscription_id, "/subscriptions/") ? var.subscription_id : "/subscriptions/${var.subscription_id}"
 }
 
-## Azure Policy Assignment
+data "azurerm_policy_definition" "built_in" {
+  display_name = local.policy_display_name
+}
+
 resource "azurerm_subscription_policy_assignment" "policy_assignment" {
   name                 = var.name
   display_name         = var.name
   description          = var.description
-  subscription_id      = var.subscription_id
-  policy_definition_id = local.policy_definition_id
+  subscription_id      = local.subscription_resource_id
+  policy_definition_id = data.azurerm_policy_definition.built_in.id
+
   parameters = jsonencode(var.parameters)
 
   lifecycle {
     precondition {
-      condition     = local.policy_definition_id != null
+      condition     = local.policy_display_name != null
       error_message = "Unsupported policy_type '${var.policy_type}'. Supported: allowed_locations, require_tag_on_resources."
     }
   }
 }
-
