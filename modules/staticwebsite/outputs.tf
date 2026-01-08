6ee8a757ebcb0066317093edfea6b0569b0c69e1
gbene <bene.gabriel3@gmail.com>
2026-01-08 16:49:00 +0200

    adding workflow for static content

--- modules/staticwebsite/outputs.tf
+++ modules/staticwebsite/outputs.tf
@@ -12,3 +12,14 @@ output "cdn_endpoint_hostname" {
   description = "CDN endpoint hostname."
   value       = (var.enabled && try(var.cdn.enabled, false)) ? azurerm_cdn_endpoint.cdn_endpoint[0].fqdn : null
 }
+
+output "storage_account_name" {
+  description = "Storage account name for the static website."
+  value       = var.enabled ? azurerm_storage_account.static_site[0].name : null
+}
+
+output "storage_account_primary_access_key" {
+  description = "Primary access key (used for uploads)."
+  value       = var.enabled ? azurerm_storage_account.static_site[0].primary_access_key : null
+  sensitive   = true
+}
